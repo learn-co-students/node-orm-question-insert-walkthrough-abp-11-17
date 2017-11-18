befe48125aca57e2e68967865809d4816b4709c4
harenarium <carolinel2610@gmail.com>
2017-11-18 18:33:22 +0000

    Done.

--- models/Question.js
+++ models/Question.js
@@ -7,15 +7,28 @@ class Question{
         id INTEGER PRIMARY KEY,
         content TEXT
       )`
-      
+
       db.run(sql, function(){
         resolve("questions table created")
-      })      
+      })
     })
   }
 
   constructor(content){
     this.content = content
+  }
+
+  insert(){
+
+     const sql = 'INSERT INTO questions (content) VALUES (?)' //need () around ?
+     const self = this
+
+    return new Promise(function(resolve){ //promise(function(resolve){}) two sets of ()
+       db.run(sql,[self.content],function(){ //don't need interpolation around self.content
+         self.id = this.lastID  // this.lastID not this.id
+         resolve(self)
+       })
+    })
   }
 
 }
