68ca16a8c1dce9dce13f681d7b649e97f1375c84
M <marlonsaddress@gmail.com>
2017-11-19 15:41:54 +0000

    Done.

--- models/Question.js
+++ models/Question.js
@@ -1,23 +1,33 @@
-const db = require("../config/db")
+const db = require('../config/db');
 
-class Question{
+class Question {
   static CreateTable() {
-    return new Promise(function(resolve){
+    return new Promise(function(resolve) {
       const sql = `CREATE TABLE questions (
         id INTEGER PRIMARY KEY,
         content TEXT
-      )`
-      
-      db.run(sql, function(){
-        resolve("questions table created")
-      })      
-    })
+      )`;
+
+      db.run(sql, function() {
+        resolve('questions table created');
+      });
+    });
   }
 
-  constructor(content){
-    this.content = content
+  constructor(content) {
+    this.content = content;
   }
 
+  insert(){
+    const self = this // THIS IS THE CRUX
+    const sql = `INSERT INTO questions (content) VALUES (?)`
+    return new Promise(function(resolve){
+      db.run(sql, [self.content], function(err, result){
+        self.id = this.lastID
+        resolve(self)  
+      });
+    });
+  }
 }
 
 module.exports = Question;
